f793d09799b87c9129913e40dbd5491f6781d4f1
KyCem <cemkayabsnss@gmail.com>
2023-08-13 01:24:14 +0300

    Code cleaned

---
+++ constants.py
@@ -0,0 +1,47 @@
+arrival_variant = [{"city" :"prag" ,"city_code" : "prga"},     {"city" :"amsterdam" ,"city_code" : "amsa"},
+                   {"city" :"paris" ,"city_code" :  "para"},   {"city" :"milano" ,"city_code" : "mila"}, 
+                   {"city" :"barselona","city_code" :  "bcna"},{"city" :"berlin" ,"city_code" : "bera"}, 
+                   {"city" :"floransa" ,"city_code" : "flra"}, {"city" :"roma" ,"city_code" : "roma"},  
+                   {"city" :"venedik" ,"city_code" : "vcea"},  {"city" :"lyon" ,"city_code" : "lysa"},  
+                   {"city" :"kopenhag" ,"city_code" : "cpha"}, {"city" :"bruksel" ,"city_code" : "brua"}, 
+                   {"city" :"frankfurt" ,"city_code" : "fraa"},{"city" :"munih" ,"city_code" : "muca"}, 
+                   {"city" :"madrid" ,"city_code" : "mada"},   {"city" :"viyana" ,"city_code" : "viea"}, 
+                 ]
+oneway_departure_date_variants = ["02.09.2023","03.09.2023","04.09.2023","05.09.2023","06.09.2023","07.09.2023",
+                                  "08.09.2023","09.09.2023","10.09.2023","11.09.2023","12.09.2023","13.09.2023",
+                                  "14.09.2023","15.09.2023","16.09.2023","17.09.2023","18.09.2023","19.09.2023",
+                                  ]
+oneway_arrival_date_variants = [
+                                "23.09.2023","24.09.2023","25.09.2023",
+                                ]
+oneway_departure_date_variants_semester = [
+                                  "14.01.2024","15.01.2024","16.01.2024","17.01.2024","18.01.2024","19.01.2024",
+                                  "20.01.2024","21.01.2024","22.01.2024","23.01.2024","24.01.2024","25.01.2024"
+                                  ]
+
+
+def generateFlightInfo(to_location_city, to_location,oneway_departure_date):
+    flight_info ={
+        "from_location_city1" : 'istanbul',
+        "from_location_city" : 'sofia-havalimani',
+              "to_location_city" : to_location_city,
+              "from_location1" :  'ista',
+              "from_location" :  'sof',
+              "to_location" : to_location,
+              "departure_date" : '02.09.2023',
+              "arrival_date" : '14.09.2023',
+              "oneway_departure_date" : oneway_departure_date
+              }
+    return flight_info
+
+def giveUrl(flight_info,urlType):
+    departureUrl = "https://www.enuygun.com/ucak-bileti/{flight_info[from_location_city1]}-{flight_info[to_location_city]}-{flight_info[from_location1]}-{flight_info[to_location]}/?gidis={flight_info[oneway_departure_date]}&yetiskin=1&sinif=ekonomi&save=1&geotrip=international&trip=international".format(flight_info = flight_info)
+    arrivalUrl = "https://www.enuygun.com/ucak-bileti/{flight_info[to_location_city]}-{flight_info[from_location_city1]}-{flight_info[to_location]}-{flight_info[from_location1]}/?gidis={flight_info[oneway_departure_date]}&yetiskin=1&sinif=ekonomi&save=1&geotrip=international&trip=international".format(flight_info = flight_info)
+    twoWayUrl = "https://www.enuygun.com/ucak-bileti/{flight_info[from_location_city]}-{flight_info[to_location_city]}-{flight_info[from_location]}-{flight_info[to_location]}/?gidis={flight_info[departure_date]}&donus={flight_info[arrival_date]}&yetiskin=1&sinif=ekonomi&save=1&geotrip=international&trip=international".format(flight_info = flight_info)
+    if urlType == "arrival":
+        return arrivalUrl
+    elif urlType == "departure":
+        return departureUrl
+    elif urlType == "twoWay":
+        return twoWayUrl
+
--- scraping.py
+++ scraping.py
@@ -8,58 +8,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import os
 import email_sender as email_sender
+import constants
 
 options = Options()
 PROXY = "103.49.202.252:80"
 options.add_argument('--proxy-server=%s' % PROXY)
 driver = uc.Chrome()
 
-arrival_variant = [{"city" :"amsterdam" ,"city_code" : "amsa"},{"city" :"paris" ,"city_code" :  "para"},
-                   {"city" :"barselona","city_code" :  "bcna"},{"city" :"berlin" ,"city_code" : "bera"}, 
-                   {"city" :"prag" ,"city_code" : "prga"},     {"city" :"milano" ,"city_code" : "mila"}, 
-                   {"city" :"floransa" ,"city_code" : "flra"}, {"city" :"roma" ,"city_code" : "roma"},  
-                   {"city" :"venedik" ,"city_code" : "vcea"},  {"city" :"lyon" ,"city_code" : "lysa"},  
-                   {"city" :"kopenhag" ,"city_code" : "cpha"},{"city" :"bruksel" ,"city_code" : "brua"}, 
-                   {"city" :"frankfurt" ,"city_code" : "fraa"},{"city" :"munih" ,"city_code" : "muca"}, 
-                   {"city" :"madrid" ,"city_code" : "mada"},   {"city" :"viyana" ,"city_code" : "viea"}, 
-                 ]
-oneway_departure_date_variants = ["02.09.2023","03.09.2023","04.09.2023","05.09.2023","06.09.2023","07.09.2023",
-                                  "08.09.2023","09.09.2023","10.09.2023","11.09.2023","12.09.2023","13.09.2023",
-                                  "14.09.2023","15.09.2023","16.09.2023","17.09.2023","18.09.2023","19.09.2023",
-                                  "20.09.2023","21.09.2023","22.09.2023","23.09.2023","24.09.2023","25.09.2023"]
-
-oneway_departure_date_variants_semester = [
-                                  "14.01.2024","15.01.2024","16.01.2024","17.01.2024","18.01.2024","19.01.2024",
-                                  "20.01.2024","21.01.2024","22.01.2024","23.01.2024","24.01.2024","25.01.2024"]
-def find_cheapest_flight(to_location_city, to_location,oneway, oneway_departure_date):
-    flight_info ={"from_location_city" : 'istanbul',
-              "to_location_city" : to_location_city,
-              "from_location" :  'ista',
-              "to_location" : to_location,
-              "departure_date" : '02.09.2023',
-              "arrival_date" : '14.09.2023',
-              "oneway_departure_date" : oneway_departure_date
-              }
-    if oneway:
-        url = "https://www.enuygun.com/ucak-bileti/{flight_info[from_location_city]}-{flight_info[to_location_city]}-{flight_info[from_location]}-{flight_info[to_location]}/?gidis={flight_info[oneway_departure_date]}&yetiskin=1&sinif=ekonomi&save=1&geotrip=international&trip=international".format(flight_info = flight_info)
-    else:
-        url = "https://www.enuygun.com/ucak-bileti/{flight_info[from_location_city]}-{flight_info[to_location_city]}-{flight_info[from_location]}-{flight_info[to_location]}/?gidis={flight_info[departure_date]}&donus={flight_info[arrival_date]}&yetiskin=1&sinif=ekonomi&save=1&geotrip=international&trip=international".format(flight_info = flight_info)
-
+def find_cheapest_flight(to_location_city, to_location,flightType, oneway_departure_date):
+    flight_info = constants.generateFlightInfo(to_location_city, to_location, oneway_departure_date)
+    url = constants.giveUrl(flight_info, flightType)
     driver.get(url)
-    sleep(7)
+    sleep(11)
     #driver.find_element(By.XPATH, cheapest_button).click()
 
     #Getting the first x flight ticketresults
     flight_rows = []
     for i in range(2):
-        if oneway:
+        if flightType == "arrival" or flightType == "departure":
             id = 'flight-' + str(i)
             flight_row = driver.find_elements(By.ID, id)
         else:
             class_name = 'matrix_parent_' + str(i)
             flight_row = driver.find_elements(By.CLASS_NAME, class_name)
         flight_rows.append(flight_row)
-    sleep(1)
+    sleep(1.6)
 
     #Insepting the data from flight result rows then getting the data we want
     price_list = []
@@ -69,7 +42,7 @@ def find_cheapest_flight(to_location_city, to_location,oneway, oneway_departure_
             elementSoup = BeautifulSoup(elementHTML, 'html.parser')
         
             #Price
-            if oneway:
+            if flightType == "arrival" or flightType == "departure":
                 #temp_price = elementSoup.find("div", {'class' : 'summary-average-price'})
                 temp_price = elementSoup.find("span", {'class' : 'money-int'})
 
@@ -80,26 +53,23 @@ def find_cheapest_flight(to_location_city, to_location,oneway, oneway_departure_
     return [price_list, flight_info]
 
 
-for location_data in arrival_variant:
+for location_data in constants.arrival_variant:
+    date_variants = constants.oneway_departure_date_variants
     print(location_data['city'] + " "+ location_data["city_code"])
     price_list_with_dates = []
-    for i in range(len(oneway_departure_date_variants)):
-        date= oneway_departure_date_variants[i]
-        return_list = find_cheapest_flight(location_data['city'],location_data["city_code"],True, date)
-        if return_list is not None:
-            if len(return_list[0]) > 0:
-                if float(return_list[0][0]) < 2.500 and return_list[0][0] is not None:
-                    print(return_list[0][0] + "Uygun Fiyat")
-                    price_list_with_dates.append("date: " + date +" -> "+ "price: " + str(return_list[0]))
-        # if   return_list[0] is not None  and float(return_list[0][0]) < 4.100:
-            # print(return_list[0][0] + "Uygun Fiyat")
-            # price_list_with_dates.append("date: " + date +" -> "+ "price: " + str(return_list[0]))
-        # print(return_list[0])
-        # price_list_with_dates.append("date: " + date +" -> "+ "price: " + str(return_list[0]))
+    for i in range(len(date_variants)):
+        return_list = find_cheapest_flight(location_data['city'],location_data["city_code"],"arrival", date_variants[i])
+        if return_list is not None and len(return_list[0]) > 0 and float(return_list[0][0]) < 2.600 and return_list[0][0] is not None :
+            print(return_list[0][0] + "Uygun Fiyat")
+            price_list_with_dates.append("date: " + date_variants[i] +" -> "+ "price: " + str(return_list[0]))
     #Sending the data to email
-    subject = "ISTANBUL -> " + location_data['city'].upper()
+    subject =  location_data['city'].upper() + " -> " + "ISTANBUL" 
     body = str(price_list_with_dates)
-    email_sender.sendEmail(subject,body) 
+    if len(price_list_with_dates) > 0: 
+        email_sender.sendEmail(subject,body) 
 
+def optimizerAlogrithm():
+    return 5
 
-
+def compareWithSofiaFlight():
+    return 3
